f0524a4b20a6768533545a4ca44308a08a668934
Julio Hernández <julio458garrix@gmail.com>
2021-02-04 21:39:03 -0600

    Modificacion del HolaMundo

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App{
 
     public static void main(String args[]){
-         System.out.println("Hola Mundo en Java!!");
+         System.out.println("Hola Mundo desde vscode en Java!!");
     }
     
 }
