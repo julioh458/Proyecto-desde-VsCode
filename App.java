60cea0e6c502f41ecf562683906ba907b9870ea1
Julio Hernández <julio458garrix@gmail.com>
2021-02-04 21:41:28 -0600

    Modificacion 2 de hola mundo en model

--- App.java
+++ App.java
@@ -1,7 +1,9 @@
 public class App{
 
     public static void main(String args[]){
+        //Despliega un hola mundo
          System.out.println("Hola Mundo desde vscode en Java!!");
+         //Termina
     }
     
 }
